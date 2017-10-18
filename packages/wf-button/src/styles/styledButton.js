6487a9a97e8814fc1948c7e78967cafb0901e5ca
Gary Meehan <garymeehan@outlook.com>
2017-10-18 19:38:32 +0100

    Tidy up

--- packages/wf-button/src/styles/styledButton.js
+++ packages/wf-button/src/styles/styledButton.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export default styled.button`
   font-size: 1em;
   margin: 1em;
-  padding: 0.25em 1em;
+  padding: 0.26em 1em;
   border-radius: 3px;
   background: none;
   color: ${props => props.theme.primary};
